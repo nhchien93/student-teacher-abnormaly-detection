53990ad8e8040b642bf8ad541dd172f96d449285
ChienNH2 <nhchienqn@gmail.com>
2020-11-28 08:23:06 +0700

    update result

--- src/demo.py
+++ src/demo.py
@@ -22,7 +22,8 @@ if __name__ == '__main__':
 
     student_model = torch.load('../trained_model/{}'.format(args['model']), map_location=config.DEVICE)
 
-    img_path = '../data/carpet/test/metal_contamination/{}'.format(args['img'])
+    img_path = '../data/{}'.format(args['img'])
+    print(img_path)
     img = utils.load_image(img_path)
 
     teacher_features, teacher_feature_mean = utils.predict(img=img, model=teacher_model, mean=True, device=config.DEVICE)
@@ -41,32 +42,9 @@ if __name__ == '__main__':
     
     plt.subplot(1, 2, 2)
     plt.imshow(err, cmap='jet')
+    plt.title('Predict heatmap')
     plt.colorbar(extend='both')
     
     plt.show()
-    fig.savefig('../result/error_{}'.format(img_path.split('/')[-1]))
-    
-    # utils.plot_features(teacher_features, '../result/teacher_{}'.format(img_path.split('/')[-1]), 1, True)
-    # utils.plot_features(student_features, '../result/student_{}'.format(img_path.split('/')[-1]), 1, True)
-    
-
-    # teacher_feature_np = teacher_features.to(config.DEVICE).detach().numpy()
-    # student_feature_np = student_features.to(config.DEVICE).detach().numpy()
-
-    # error = np.abs(np.subtract(teacher_feature_np,student_feature_np))
-    # # error = np.mean(error, 1)
-
-    # error_mask = error[0]
-
-    # ori_img = utils.load_image_(img_path, (128, 128))
-    # fig = plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(ori_img)
-    # plt.title('Original Image')
-    
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(error_mask, cmap='jet')
-    # plt.title('Predict Result')
-    # plt.colorbar(extend='both')
-    # plt.show()
     # fig.savefig('../result/error_{}'.format(img_path.split('/')[-1]))
+    fig.savefig('../result/demo_sample.png')
