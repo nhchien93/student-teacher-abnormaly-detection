b73d44d2e47adac305c6ab12ab94bb8f9102dc5c
unknown <nhchienqn@gmail.com>
2021-01-07 21:59:38 +0700

    'update'

--- src/demo.py
+++ src/demo.py
@@ -15,51 +15,62 @@ import config
 arg = argparse.ArgumentParser()
 arg.add_argument("-i", "--img", required=True, help="Choose image to predict.")
 arg.add_argument("-m", "--model", required=True, help="Choose model to predict.")
-arg.add_argument("-t", "--thresh", default=0.3, type=float, help="Choose threshold to predict.")
 args = vars(arg.parse_args())
 
 if __name__ == '__main__':
     teacher_model = TeacherModel().model
 
     model_file_names = os.listdir(config.MODEL_FOLDER)
-    student_model_paths = [os.path.join(config.MODEL_FOLDER, name) for name in model_file_names if (args['model'] in name)]
-    students_model = [torch.load(student, map_location=config.DEVICE) for student in student_model_paths]
+    model_file_paths = [os.path.join(config.MODEL_FOLDER, name) for name in model_file_names if (args['model'] in name)]
 
-    img_path = '../data/{}'.format(args['img'])
+    students_model = [torch.load(student, map_location=config.DEVICE) for student in model_file_paths]
+
+    img_path = '../input/{}'.format(args['img'])
     print(img_path)
     img = utils.load_image(img_path)
-    errs = []
+
+    student_list = []
+    teacher_features = utils.predict(img=img, model=teacher_model, mean=False, device=config.DEVICE)
+    teacher_features = teacher_features.cpu().detach().numpy()
+    print('Shape teacher_features: ', teacher_features.shape)
+
     for idx, student_model in enumerate(students_model):
-        teacher_features, teacher_feature_mean = utils.predict(img=img, model=teacher_model, mean=True, device=config.DEVICE)
-        teacher_features = teacher_features.to(config.DEVICE).detach().numpy()
-        
-        student_features, student_feature_mean = utils.predict(img=img, model=student_model, mean=True, device=config.DEVICE)
-        student_features = student_features.to(config.DEVICE).detach().numpy()
-        
-        err_temp = utils.get_error(teacher_features, student_features)
-        errs.append(err_temp)
-    err = sum(errs)/len(errs)
-    result = np.where((err<args['thresh']), 0, 1)
-    one_indices = [index for index, value in np.ndenumerate(result) if value == 1]
+      
+      student_features = utils.predict(img=img, model=student_model, mean=False, device=config.DEVICE)
+      student_features = student_features.cpu().detach().numpy()
 
+      student_list.append(student_features)
+
+    student_out = sum(student_list)/len(student_list)
+    
+    student_plot = np.mean(student_out, axis=1)
+    teacher_plot = np.mean(teacher_features, axis=1)
+
+    err = utils.get_error(teacher_features, student_out)
+    
     ori_img = utils.load_image_(img_path, (128, 128))
-
-    ori_img_copy = np.copy(ori_img)
-    for idx in one_indices:
-        ori_img_copy[idx[0],idx[1]] = 0
-    fig = plt.figure(figsize = (8., 4.8))
-    plt.subplot(1, 3, 1)
+    fig = plt.figure()
+    plt.subplot(1, 4, 1)
     plt.imshow(ori_img)
     plt.title('Original Image')
+    plt.axis('off')
 
-    plt.subplot(1, 3, 2)
-    plt.imshow(ori_img_copy)
-    plt.title('Result Image (p={})'.format(args['thresh']))
+    plt.subplot(1, 4, 2)
+    plt.imshow(teacher_plot[0])
+    plt.title('Teacher Feature')
+    plt.axis('off')
+
+    plt.subplot(1, 4, 3)
+    plt.imshow(student_plot[0])
+    plt.title('Student Feature')
+    plt.axis('off')
     
-    plt.subplot(1, 3, 3)
+    plt.subplot(1, 4, 4)
     plt.imshow(err, cmap='jet')
     plt.title('Predict heatmap')
     plt.colorbar(extend='both')
+    plt.axis('off')
     
     plt.show()
-    fig.savefig(os.path.join(config.RESULT_FOLDER, 'result.png'))
+    # fig.savefig('../result/error_{}'.format(img_path.split('/')[-1]))
+    fig.savefig('../result/heat_map.png')
